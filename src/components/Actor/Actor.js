ab5a948409d5772f28dee1d3a007b41ecc7f466f
Orokana seikō <94075384+app1e1over@users.noreply.github.com>
2023-09-30 17:41:01 +0300

    one more

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -5,7 +5,7 @@ function Actor({name, character, profile_path}) {
     
     return (
         <li>
-            <img src={getImage(profile_path, 200)}></img>
+            <img src={getImage(profile_path, 200)} alt={name}></img>
             <h3>{name}</h3>
             <p>Character: {character}</p>
         </li>
